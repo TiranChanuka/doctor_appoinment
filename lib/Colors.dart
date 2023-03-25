116c4a3dd39c979068e5a3cd801c29db3a9ad79d
Tiran Chanuka <tiranchanuka1998@gmail.com>
2023-03-25 22:28:47 +0530

    Update Colors.dart

--- lib/Colors.dart
+++ lib/Colors.dart
@@ -2,4 +2,5 @@ import 'package:flutter/material.dart';
 const Color wColor=Colors.white;
 const Color bColor=Colors.black;
 const Color pColor=Color(0xFF5089FD);
+const Color sColor=Colors.black12;
 
