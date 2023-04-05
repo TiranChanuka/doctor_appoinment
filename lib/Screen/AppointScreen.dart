32e350a5c04bf173497878cb3e0c5c740231c95d
Tiran Chanuka <tiranchanuka1998@gmail.com>
2023-04-05 22:24:36 +0530

    Update AppointScreen.dart

--- lib/Screen/AppointScreen.dart
+++ lib/Screen/AppointScreen.dart
@@ -1,6 +1,7 @@
 import 'package:doctor_appoinment/Colors.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class AppointScreen extends StatelessWidget {
   const AppointScreen({Key? key}) : super(key: key);
@@ -14,12 +15,10 @@ class AppointScreen extends StatelessWidget {
           children: [
             Container(
               width: MediaQuery.of(context).size.width,
-              height: MediaQuery.of(context).size.height/2.1,
+              height: MediaQuery.of(context).size.height / 2.1,
               decoration: BoxDecoration(
                 image: DecorationImage(
-                  image: AssetImage(
-                    "images/doctor1.jpg"
-                  ),
+                  image: AssetImage("images/doctor1.jpg"),
                   fit: BoxFit.cover,
                 ),
                 borderRadius: BorderRadius.only(
@@ -43,12 +42,15 @@ class AppointScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Padding(padding: EdgeInsets.only(top: 30,left: 10,right: 10),
+                    Padding(
+                      padding: EdgeInsets.only(top: 30, left: 10, right: 10),
                       child: Row(
                         mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
                           InkWell(
-                            onTap: (){},
+                            onTap: () {
+                              Navigator.pop(context);
+                            },
                             child: Container(
                               margin: EdgeInsets.all(8),
                               height: 45,
@@ -65,7 +67,8 @@ class AppointScreen extends StatelessWidget {
                                 ],
                               ),
                               child: Center(
-                                child:Icon(Icons.arrow_back,
+                                child: Icon(
+                                  Icons.arrow_back,
                                   color: pColor,
                                   size: 28,
                                 ),
@@ -107,20 +110,252 @@ class AppointScreen extends StatelessWidget {
                           Column(
                             mainAxisAlignment: MainAxisAlignment.center,
                             children: [
-                              Text("Patients",
+                              Text(
+                                "Patients",
                                 style: TextStyle(
                                   fontSize: 20,
                                   fontWeight: FontWeight.bold,
                                   color: wColor,
                                 ),
-                              )
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                "1.8k",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500,
+                                  color: wColor,
+                                ),
+                              ),
                             ],
-                          )
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Experience",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: wColor,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                "15 years",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500,
+                                  color: wColor,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Ratings",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: wColor,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                "4.5",
+                                style: TextStyle(
+                                  fontSize: 17,
+                                  fontWeight: FontWeight.w500,
+                                  color: wColor,
+                                ),
+                              ),
+                            ],
+                          ),
                         ],
                       ),
                     ),
                   ],
                 ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Dr Sarah Johnson",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 28,
+                      color: pColor,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 5,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        MdiIcons.heartPlus,
+                        color: Colors.red,
+                        size: 28,
+                      ),
+                      Text(
+                        "Surgeon",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: bColor.withOpacity(0.6),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                      "Dr. Sarah Johnson has been working as a medical doctor for the past 15 years. She completed her medical degree at a prestigious university and then went on to complete her residency in internal medicine",
+                    style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.w500,
+                      color: Colors.black.withOpacity(0.6),
+                    ),
+                    textAlign: TextAlign.justify,
+                  ),
+                  SizedBox(height: 15,),
+                  Text(
+                    "Book date",
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: bColor.withOpacity(0.6),
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 8,),
+                  Container(
+                    height: 65,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 6,
+                      itemBuilder: (context,index){
+                        return InkWell(
+                              child: Container(
+                                margin: EdgeInsets.symmetric(horizontal: 5,vertical: 5),
+                                padding: EdgeInsets.symmetric(vertical: 11,horizontal: 25),
+                                decoration: BoxDecoration(
+                                  color: index==1 ? pColor:Color(0xFFF2F8FF),
+                                  borderRadius: BorderRadius.circular(10),
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: sColor,
+                                      blurRadius: 4,
+                                      spreadRadius: 2,
+                                    ),
+                                  ],
+                                ),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    Text(
+                                      "${index+8}",
+                                      style: TextStyle(
+                                        color: index==1 ?wColor:bColor.withOpacity(0.6),
+                                      ),
+                                    ),
+
+                                    Text(
+                                      "APR",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.w500,
+                                        color: index==1 ?wColor:bColor.withOpacity(0.6),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 5),
+                  Text(
+                    "Book Time",
+                    style: TextStyle(
+                      fontSize: 17,
+                      color: bColor.withOpacity(0.6),
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Container(
+                    height: 60,
+                    child: ListView.builder(
+                      shrinkWrap: true,
+                      scrollDirection: Axis.horizontal,
+                      itemCount: 6,
+                      itemBuilder: (context,index){
+                        return Container(
+                          margin: EdgeInsets.symmetric(horizontal: 8,vertical: 5),
+                          padding: EdgeInsets.symmetric(vertical: 10,horizontal: 5),
+                          decoration: BoxDecoration(
+                            color: index==2? pColor:Color(0xFFF2F8FF),
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: [
+                              BoxShadow(
+                                color: sColor,
+                                blurRadius: 4,
+                                spreadRadius: 2,
+                              ),
+                            ],
+                          ),
+                          child: Center(
+                            child: Text(
+                              "${index+6}:00 AM",
+                              style: TextStyle(
+                                fontSize: 17,
+                                color: index == 2? wColor:bColor.withOpacity(0.6),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  Material(
+                    color: pColor,
+                    borderRadius: BorderRadius.circular(10),
+                    child: InkWell(
+                      onTap: (){},
+                      child: Container(
+                        height: 60,
+                        width: MediaQuery.of(context).size.width,
+                        child: Center(
+                          child: Text(
+                            "Book Appoinment",
+                            style: TextStyle(
+                              color: wColor,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
               ),
             ),
           ],
